d3c8a6f7630bed6a84ce1ca2e760fbaf0f6269a3
rahad06 <dararah06@gmail.com>
2023-09-20 03:12:08 +0330

    excel 2

---
+++ ASPBPCPANELALPHA/Migrations/20230919221916_market research.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPBPCPANELALPHA.Migrations
+{
+    public partial class marketresearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MarketResearch",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarketResearch",
+                table: "Companies");
+        }
+    }
+}
